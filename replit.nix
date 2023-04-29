eb1165ef4a1f427c5599c932c5c482ccd241bf98
unknown81311 <43104779+unknown81311@users.noreply.github.com>
2023-04-29 00:49:02 -0700

    Add files via upload

---
+++ replit.nix
@@ -0,0 +1,8 @@
+{ pkgs }: {
+	deps = [
+		pkgs.nodejs-18_x
+    pkgs.nodePackages.typescript-language-server
+    pkgs.yarn
+    pkgs.replitPackages.jest
+	]; env = { LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [pkgs.libuuid]; };
+}
